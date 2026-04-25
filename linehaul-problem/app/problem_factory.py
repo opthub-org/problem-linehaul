c8a735cae3ad3f15fc595c5b28768747742f290f
kentakojima <kenta.int.tech@gmail.com>
2026-04-25 09:09:13 +0900

    fix: remove non-JSON output from evaluator stdout

--- linehaul-problem/app/problem_factory.py
+++ linehaul-problem/app/problem_factory.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-from .test_problem.problem import Problem
-from .real_problem.real_problem import RealProblem
 from .real_problem.real_cost_evaluator import RealCostEvaluator
+from .real_problem.real_problem import RealProblem
+from .test_problem.problem import Problem
 
 
 class ProblemFactory:
-
     @staticmethod
     def create(case_type: str, file_path: Path) -> Problem:
         if case_type == "test":
@@ -16,15 +15,11 @@ class ProblemFactory:
         match case_type:
             case "test":
                 variable = file_path.read_text()
-                problem = RealProblem(str_variable=variable,
-                                      evaluator=RealCostEvaluator(v_capa=100000),
-                                      n_node=46 * 46)
+                problem = RealProblem(str_variable=variable, evaluator=RealCostEvaluator(v_capa=100000), n_node=46 * 46)
 
             case "problem":
-                variable = input("input?")
-                problem = RealProblem(str_variable=variable,
-                                      evaluator=RealCostEvaluator(v_capa=100000),
-                                      n_node=46 * 46)
+                variable = input()
+                problem = RealProblem(str_variable=variable, evaluator=RealCostEvaluator(v_capa=100000), n_node=46 * 46)
             case _:
                 raise ValueError(f"case_type:{case_type} is undifined")
         return problem
